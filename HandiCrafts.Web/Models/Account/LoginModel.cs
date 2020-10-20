83e7bb3009bb06e269f81d102bd3f47753e82d69
Esmaeil.Vahedi.Co <vahedi.esmaeil@outlook.com>
2020-10-20 12:59:33 +0330

    Add effect to baner

--- HandiCrafts.Web/Models/Account/LoginModel.cs
+++ HandiCrafts.Web/Models/Account/LoginModel.cs
@@ -5,6 +5,7 @@ namespace HandiCrafts.Web.Models.Account
 {
     public class LoginContainerModel
     {
+        public ShortLoginModel ShortLoginModel { get; set; }
         public LoginModel LoginModel { get; set; }
         public RegisterModel RegisterModel { get; set; }
         public ForgotPasswordModel ForgotPasswordModel { get; set; }
@@ -14,17 +15,26 @@ namespace HandiCrafts.Web.Models.Account
     {
         [StringLength(100, MinimumLength = 3, ErrorMessage = "حداکثر طول ایمیل 100 و حداقل 3 کارکتر می باشد")]
         [Required(ErrorMessage = "ورود نام کاربری اجباری می باشد")]
-        [Display(Name ="نام کاربری")]
+        [Display(Name = "نام کاربری")]
         public string Username { get; set; }
 
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "رمز عبور اجباری است")]
         [Display(Name = "رمزعبور")]
         public string Password { get; set; }
-        
+
         public string ReturnUrl { get; set; }
 
         public bool RememberMe { get; set; }
         public string ReCaptchaKey { get; set; }
+    }
+
+    public class ShortLoginModel
+    {
+        [Display(Name = "شماره موبایل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(11, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        [RegularExpression("09(0[1-9]|[0-9][0-9]|3[0-9]|2[0-1])-?[0-9]{3}-?[0-9]{4}", ErrorMessage = "شماره موبایل معتبر نمی باشد")]
+        public string MobileNo { get; set; }
     }
 }
